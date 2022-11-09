efc87c7b4e8617036618d7ee72fb0f690b87eaa2
mr.zyablik <danilsautenkov27@gmail.com>
2022-11-09 18:58:09 +0300

    First commit

---
+++ game.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def random_predict(number: int=1)->int:
+    """рандомно угадываем число
+
+    Args:
+        number (int, optional): загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
